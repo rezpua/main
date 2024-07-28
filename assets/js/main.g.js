67b7516986b4d146a8007bf46641ad8c1b193501
Vasyl Mosiychuk <132908196+rezpua@users.noreply.github.com>
2024-07-28 20:29:33 +0300

    Change #46

---
+++ assets/js/main.g.js
@@ -0,0 +1,10 @@
+---
+layout: null
+sitemap: false
+---
+{%- include js/jquery.min.js -%}
+{%- include js/popper.min.js -%}
+{%- include js/bootstrap.min.js -%}
+{%- include js/bootstrap.smoothscroll.min.js -%}
+{%- include js/tom-select.base.min.js -%}
+{%- include js/realtyua.js -%}
